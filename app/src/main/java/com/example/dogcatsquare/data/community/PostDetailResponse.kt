5b0898c1876c672447aff15dcbae413d2817e770
2cmin <dlcoals7047@nate.com>
2025-02-13 19:12:41 +0900

    feat : 게시글 상세 조회

---
+++ app/src/main/java/com/example/dogcatsquare/data/community/PostDetailResponse.kt
@@ -0,0 +1,9 @@
+package com.example.dogcatsquare.data.community
+
+data class PostDetailResponse(
+    val isSuccess: Boolean,
+    val code: String,
+    val message: String,
+    val result: PostDetail?
+)
+
