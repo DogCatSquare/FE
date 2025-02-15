8434487b9f513d3b85dd24a3981d3b143d9bb24c
answjddn0607 <166084634+answjddn0607@users.noreply.github.com>
2025-02-15 22:13:55 +0900

    Feat: 추가 산책로 조회 api
    산책로 조회 api를 연결하였습니다.
    
    기존 코드에선 loadAllCategory() 함수를 통해 장소에 대한 데이터를 불러오고, 해당 데이터를 리스트에 추가한 뒤 RV에 연결하였습니다.
    이를 loadPlacesData(), updateUI(), loadAllCategiry()로 분리하였습니다.
    
    loadWalkData() 함수를 추가하여 산책로 데이터를 불러올 수 있도록 하였습니다.
    loadAllCategory()에서 loadPlacesData(), loadWalkData()를 통해 불러온 데이터를 합치고, updateUI()를 통해 합친 데이터를 rv에 연결하도록 하였습니다.
    
    loadWalkData()를 구현하기 위해 WalkResponse.kt 파일을 만들었고, MapPlace 및 PlaceApiService 파일을 수정하였습니다.

---
+++ app/src/main/java/com/example/dogcatsquare/data/map/WalkResponse.kt
@@ -0,0 +1,46 @@
+package com.example.dogcatsquare.data.map
+
+import com.google.gson.annotations.SerializedName
+
+data class WalkListRequest(
+    @SerializedName("latitude") val latitude: Double,
+    @SerializedName("longitude") val longitude: Double,
+    @SerializedName("radius") val radius: Double
+)
+
+data class WalkListResponse(
+    @SerializedName("walks") val walks: List<Walk>
+)
+
+data class Walk(
+    @SerializedName("walkId") val walkId: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("walkImageUrl") val walkImageUrl: List<String>,
+    @SerializedName("reviewCount") val reviewCount: Int,
+    @SerializedName("distance") val distance: Double,
+    @SerializedName("time") val time: Int,
+    @SerializedName("difficulty") val difficulty: String,
+    @SerializedName("special") val special: List<Special>,
+    @SerializedName("coordinates") val coordinates: List<Coordinate>,
+    @SerializedName("createdAt") val createdAt: String,
+    @SerializedName("updatedAt") val updatedAt: String,
+    @SerializedName("createdBy") val createdBy: CreatedBy
+)
+
+data class Special(
+    @SerializedName("type") val type: String,
+    @SerializedName("customValue") val customValue: String
+)
+
+data class Coordinate(
+    @SerializedName("latitude") val latitude: Double,
+    @SerializedName("longitude") val longitude: Double,
+    @SerializedName("sequence") val sequence: Int
+)
+
+data class CreatedBy(
+    @SerializedName("nickname") val nickname: String,
+    @SerializedName("breed") val breed: String,
+    @SerializedName("profileImageUrl") val profileImageUrl: String
+)
